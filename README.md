1df490dd366ac781325579e1404b8dfb53ce1b91
ladrian <ladrian@vmware.com>
2020-05-23 17:13:57 +0800

    changed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # spring_cloud_data_flow
+
+
+http --server.port=20102| python-jython --jython.script=main.py --git.uri=https://github.com/leeadh/spring_cloud_data_flow | log
--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import json
-y = json.loads(payload)
-result = y["amount"]
+
+# comment out below line when you deploy to spring cloud data flow 
+#payload = '{ "InvoiceNo":536365, "StockCode":"85123A", "Description":"WHITE HANGING HEART T-LIGHT HOLDER", \
+#        "Quantity":6, "InvoiceDate": "12/1/2010  8:26:00 AM" , "UnitPrice": 2.55 , "BuyPrice": 3, "CustomerID": 1780, "Country": "United Kingdom"}'
+
+data = json.loads(payload)
+
+#Transforming data  - price * quantity
+TotalUnitPrice = data["Quantity"] * data["UnitPrice"]
+
+#Transforming data  - price * quantity
+TotalBuyPrice = data["Quantity"] * data["BuyPrice"]
+Deviation = TotalBuyPrice - TotalUnitPrice
+
+data["TotalUnitPrice"] = TotalUnitPrice
+data["TotalBuyPrice"]  = TotalBuyPrice
+data["Deviation"]      = Deviation
+
+result = data 
+
+ 
